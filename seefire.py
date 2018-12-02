1a651947e16bd8d2b990999929b8dd3eb7669ce3
cjs8009 <cjs8009@cs.rit.edu>
2018-12-02 17:01:37 -0500

    update

--- seefire.py
+++ seefire.py
@@ -7,7 +7,7 @@ import gui
 
 
 def getRules():
-    run(["sudo", "iptables", "-S", ">", "temp.txt"])
+    call(["sudo", "iptables", "-S", ">", "temp.txt"])
     rules = open("./temp.txt", "r")
     rulesOut = rules.read()
     rules.close()

d9b90cc8a2fe21945f367674b53d50a409c48d51
cjs8009 <cjs8009@cs.rit.edu>
2018-12-02 17:07:16 -0500

    update

--- seefire.py
+++ seefire.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # Firewall Tool By: Cristofer Sochacki
 import subprocess
+import os
 from subprocess import *
 import gui
 
 
 
 def getRules():
-    subprocess.run(["sudo", "iptables", "-S", ">", "temp.txt"])
-    rules = open("./temp.txt", "r")
+#    subprocess.run(["sudo", "iptables", "-S", ">", "temp.txt"])
+    os.system("sudo iptables -S > temp.txt")
+    rules = open("temp.txt", "r")
     rulesOut = rules.read()
     rules.close()
 #    out = rules.stdout
